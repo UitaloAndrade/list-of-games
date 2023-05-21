1a4fd8ad6e3a8414e5571adefaa08e9b7a0ee889
UitaloAndrade <uitalo.a.reis@gmail.com>
2023-05-21 20:13:15 -0300

    Homolog ok

--- src/main/java/com/uitaloandrade/list/repositories/GameRepository.java
+++ src/main/java/com/uitaloandrade/list/repositories/GameRepository.java
@@ -15,7 +15,7 @@ import com.uitaloandrade.list.projections.GameMinProjection;
 public interface GameRepository extends JpaRepository<Game, Long>{	//Camada de Acesso a dados = responsavel por acessar os dados que estao no BD
 
 	@Query(nativeQuery = true, value = """
-			SELECT tb_game.id, tb_game.title, tb_game.game_year AS `year`, tb_game.img_url AS imgUrl,
+			SELECT tb_game.id, tb_game.title, tb_game.game_year AS gameYear, tb_game.img_url AS imgUrl,
 			tb_game.short_description AS shortDescription, tb_belonging.position
 			FROM tb_game
 			INNER JOIN tb_belonging ON tb_game.id = tb_belonging.game_id
